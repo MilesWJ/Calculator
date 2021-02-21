e8853c97f05fda20d05998c7a9f140e6194d01c0
Miles James <milesw.james@gmail.com>
2021-02-20 16:37:56 -0800

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -5,39 +5,53 @@ public class calculator {
         Scanner Input = new Scanner(System.in);
 
         System.out.print("\nEnter the first number: ");
-        double num1 = Input.nextDouble();
+        double number1 = Input.nextDouble();
 
         System.out.print("Enter the second number: ");
-        double num2 = Input.nextDouble();
+        double number2 = Input.nextDouble();
 
         System.out.print(
-                "\nWhat operation would you like to perform? [1, 2, 3, 4]\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nType here: ");
+                "\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nEnter the operation number would you like to perform: ");
         int operation = Math.abs(Input.nextInt());
 
-        if (operation == 1) {
-            double answer = num1 + num2;
-            System.out.print("\nThe sum of " + num1 + " and " + num2 + " is " + answer + "!");
-        }
-
-        else if (operation == 2) {
-            double answer = num1 - num2;
-            System.out.print("\nThe difference of " + num1 + " and " + num2 + " is " + answer + "!");
-        }
-
-        else if (operation == 3) {
-            double answer = num1 * num2;
-            System.out.print("\nThe product of " + num1 + " and " + num2 + " is " + answer + "!");
-        }
-
-        else if (operation == 4) {
-            double answer = num1 / num2;
-            System.out.print("\nThe quotient of " + num1 + " and " + num2 + " is " + answer + "!");
-        }
-
-        else {
-            System.out.print("\n" + operation + " is not a valid operation selector.");
-        }
-
         Input.close();
+
+        switch (operation) {
+            case 1:
+                add(number1, number2);
+                break;
+            case 2:
+                sub(number1, number2);
+                break;
+            case 3:
+                mul(number1, number2);
+                break;
+            case 4:
+                div(number1, number2);
+                break;
+            default:
+                System.out.println("\n" + operation + " is an invalid operation...");
+                break;
+        }
+    }
+
+    public static void add(double num1, double num2) {
+        double answer = num1 + num2;
+        System.out.print("\nThe sum of " + num1 + " and " + num2 + " is " + answer + "!");
+    }
+
+    public static void sub(double num1, double num2) {
+        double answer = num1 - num2;
+        System.out.print("\nThe difference of " + num1 + " and " + num2 + " is " + answer + "!");
+    }
+
+    public static void mul(double num1, double num2) {
+        double answer = num1 * num2;
+        System.out.print("\nThe product of " + num1 + " and " + num2 + " is " + answer + "!");
+    }
+
+    public static void div(double num1, double num2) {
+        double answer = num1 / num2;
+        System.out.print("\nThe quotient of " + num1 + " and " + num2 + " is " + answer + "!");
     }
 }
